2e728501519ad5dd1abd40ac098ab818975f2041
Joao Franco <joaomfranco@gmail.com>
2018-10-14 20:39:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,10 +33,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bemvindo ao React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Para iniciar-se edite <code>src/App.js</code> e guarde para recarregar.
         </p>
         <LambdaDemo/>
       </div>
